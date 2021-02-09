3acfef65813c60580a610395981bdbc9af26c575
Laura Denney <laura.a.denney@gmail.com>
2021-02-09 07:20:14 -0800

    initial commit

---
+++ boston_linearregression
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from sklearn.datasets import load_boston
+
+%matplotlib inline
+
+boston_dataset = load_boston()
+
+#describes the dataset
+boston_dataset.DESCR
+
+print(boston_dataset.keys())
+#data is the information, target = prices of houses - dependent variable
+#feature_names is th real name of the columns
+
+boston = pd.DataFrame(boston_dataset.data, columns = boston_dataset.feature_names)
+boston.head()
+#shows top 5
+
+boston.isnull().sum()
+#checks the null values
+
+#check the distribution
+sns.set(rc={'figure.figsize':})
+
+#heatmap
+
+plt.figure(figsize=(20, 5))
+
+features == ['LSTAT', 'RM']
+target = boston['MEDV']
+
+for i, col in enumerate(features):
+    plt.subplot(1, len(features), i+1)
+    x=boston[col]
+    y = target
+    plt.scatter(x, y, marker='o')
+    plt.title(col)
+    plt.xlabel(col)
+    plt.ylabel('MEDV')
---
+++ icecream_sales.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+
+regressor = DecisionTreeRegressor()
+dataset = pd.read_csv('https://raw.githubusercontent.com/mk-gurucharan/Regression/master/IceCreamData.csv')
+
+# independent variable
+X = dataset['Temperature'].values
+# dependnet variable
+y = dataset['Revenue'].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05)
+regressor.fit(X_train.reshape(-1, 1), y_train.reshape(-1, 1))
+
+y_pred = regressor.predict(X_test.reshape(-1, 1))
+
+df = pd.DataFrame({'Real Values': y_test.reshape(-1), 'Predicted Values': y_pred.reshape(-1)})
+
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X_test, y_test, color='red')
+plt.scatter(X_test, y_pred, color='green')
+plt.title('Decision Tree Regression')
+plt.xlabel('Temperature')
+plt.ylabel('Revenue')
+plt.show()
+
+plt.plot(X_grid, regressor.predict(X_grid), color='black')
+plt.title('Decision Tree Regression')
+plt.xlabel('Temperature')
+plt.ylabel('Revenue')
+plt.show()
